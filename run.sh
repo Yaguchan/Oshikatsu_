e30b8fbfee824331d8d9da365773ac60946c0ca6
Yaguchan <guchi4867@gmail.com>
2024-08-12 10:08:25 +0900

    20240812

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 # 0.設定
-MOVNAME="62thSingleDance_178_182_1k"                                # 動画名(data/mp4/MOVNAME.mp4)
-YOLO_WEIGHTS="weights/yolov8x.pt"                                   # YOLOv8 weights
-YOLO_FACE_WEIGHTS="weights/yolov8n-face.pt"                         # YOLO face weights
-FACENET_WEIGHTS="weights/facenet_62thsingle.pt"                     # FaceNet weights
-TRACKING_YAML="data/yaml/bytetrack.yaml"                            # TrackingModel(botsort/bytetrack)
+MOVNAME="62thSingleDance_178_182_1k"                                # movie name (data/mp4/MOVNAME.mp4)
+YOLO_WEIGHTS="weights/yolov8x.pt"                                   # YOLOv8 weights (yolov8X.pt)
+YOLO_FACE_WEIGHTS="weights/yolov8n-face.pt"                         # YOLO face weights (yolov8X-face.pt)
+FACENET_WEIGHTS="weights/facenet_62thsingle.pt"                     # FaceNet weights (facenet_X.pt)
+TRACKING_YAML="data/yaml/bytetrack.yaml"                            # TrackingModel (botsort/bytetrack.yaml)
 MEMBER_LIST="face_identification/member_list/62thsingle.txt"        # member list
 MEMBER_ENJP_LIST="face_identification/member_list/member.csv"       # member en/jp list
 FONT="data/font/NotoSansJP-Black.ttf"                               # Font 

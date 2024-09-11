66b21ffd53707daf43cd2a56f6378869971928fe
Yaguchan <guchi4867@gmail.com>
2024-09-11 10:07:27 +0900

    20240911

--- run/run.sh
+++ run/run.sh
@@ -1,5 +1,5 @@
 # 0.設定
-MOVNAME="62thSingleDance_178_182_1k"                                # movie name (data/mp4/MOVNAME.mp4)
+MOVNAME="62thSingleDance_1k"                                        # movie name (data/mp4/MOVNAME.mp4)
 YOLO_WEIGHTS="weights/yolov8x.pt"                                   # YOLOv8 weights (yolov8X.pt)
 YOLO_FACE_WEIGHTS="weights/yolov8n-face.pt"                         # YOLO face weights (yolov8X-face.pt)
 FACENET_WEIGHTS="weights/facenet_62thsingle.pt"                     # FaceNet weights (facenet_X.pt)
@@ -9,10 +9,10 @@ MEMBER_ENJP_LIST="face_identification/member_list/member.csv"       # member en/
 FONT="data/font/NotoSansJP-Black.ttf"                               # Font 
 DEVICE="mps"                                                        # cuda or mps or cpu
 
-# 実行　bash run.sh
+# 実行　bash run/run.sh
 # 1.トラッキング
-python mov2ids.py --mov $MOVNAME --yolo-weights $YOLO_WEIGHTS --tracking-yaml $TRACKING_YAML
+python tracking/mov2ids.py --mov $MOVNAME --yolo-weights $YOLO_WEIGHTS --tracking-yaml $TRACKING_YAML
 # 2.顔識別
-python id2name.py --mov $MOVNAME --member-list $MEMBER_LIST --yolo-face-weights $YOLO_FACE_WEIGHTS --facenet-weights $FACENET_WEIGHTS --device $DEVICE
+python tracking/id2name.py --mov $MOVNAME --member-list $MEMBER_LIST --yolo-face-weights $YOLO_FACE_WEIGHTS --facenet-weights $FACENET_WEIGHTS --device $DEVICE
 # 3.動画作成
-python make_mov.py --mov $MOVNAME --member-enjp-list $MEMBER_ENJP_LIST --font $FONT
+python tracking/make_mov.py --mov $MOVNAME --member-enjp-list $MEMBER_ENJP_LIST --font $FONT
